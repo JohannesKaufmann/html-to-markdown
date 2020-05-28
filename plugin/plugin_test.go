073c27517ab8430462db830ee0254d4cef7f2fed
Gergely Brautigam <gergely@arangodb.com>
2020-05-28 15:43:12 +0200

    Added confluence attachment parser

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -164,3 +164,25 @@ FOR stuff IN imdb_vertices
 		t.Errorf("got '%s' but wanted '%s'", markdown, expected)
 	}
 }
+
+func TestConfluenceAttachments(t *testing.T) {
+	conv := md.NewConverter("", true, nil)
+	conv.Use(ConfluenceAttachments())
+
+	input := `<p>Here&rsquo;s an image:</p><p /><ac:image ac:align="center" ac:layout="center" ac:original-height="290" ac:original-width="290"><ri:attachment ri:filename="image.png" ri:version-at-save="1" /></ac:image><p /><p>Another one</p><ac:image ac:align="center" ac:layout="center" ac:original-height="457" ac:original-width="728"><ri:attachment ri:filename="image.jpg" ri:version-at-save="1" /></ac:image><p />`
+	expected := `Here’s an image:
+
+![][image.png]
+
+Another one
+
+![][image.jpg]`
+	markdown, err := conv.ConvertString(input)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if markdown != expected {
+		t.Errorf("got '%s' but wanted '%s'", markdown, expected)
+	}
+}
